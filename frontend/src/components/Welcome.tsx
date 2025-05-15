5b572a06af3090a4f42e8eeee0c9907dc6afea13
shabinx30 <shabeensharih@gmail.com>
2025-05-15 23:39:07 +0530

    feat: enhance styling for dark mode support in Welcome, Contact, and Contacts components

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -2,10 +2,10 @@ import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Welcome = () => {
     return (
-        <div onContextMenu={(e) => e.preventDefault()} className="flex-1/3 bg-[#dee1ff] flex flex-col justify-center items-center text-center">
-            <BiMessageSquareDetail size={50} className="mb-4" />
+        <div onContextMenu={(e) => e.preventDefault()} className="flex-1/3 bg-[#dee1ff] dark:bg-[#131313] text-black dark:text-white flex flex-col justify-center items-center text-center">
+            <BiMessageSquareDetail size={50} className="mb-4 text-[#626fff]" />
             <h1 className="font-semibold text-2xl mb-2">Welcome to Chat!</h1>
-            <p className="text-[#484848] text-[0.9em]">
+            <p className="text-[#484848] dark:text-[#868686] text-[0.9em]">
                 Select a contact to start the conversation.
             </p>
         </div>
