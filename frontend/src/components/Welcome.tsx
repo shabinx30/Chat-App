40aedd75826b8269c414e6eaaf71f19388dc1c63
shabinx30 <shabeensharih@gmail.com>
2025-05-15 23:17:48 +0530

    feat: prevent context menu on Welcome component for better user experience

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -2,7 +2,7 @@ import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Welcome = () => {
     return (
-        <div className="flex-1/3 bg-[#dee1ff] flex flex-col justify-center items-center text-center">
+        <div onContextMenu={(e) => e.preventDefault()} className="flex-1/3 bg-[#dee1ff] flex flex-col justify-center items-center text-center">
             <BiMessageSquareDetail size={50} className="mb-4" />
             <h1 className="font-semibold text-2xl mb-2">Welcome to Chat!</h1>
             <p className="text-[#484848] text-[0.9em]">
