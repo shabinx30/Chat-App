99ebc400c57a3b6d54ee9f0d656aa85c3cb1727e
shabinx30 <shabeensharih@gmail.com>
2025-05-16 10:37:03 +0530

    feat: refactor Contact component layout; separate name and timestamp into distinct elements

--- frontend/src/components/contacts/Contact.tsx
+++ frontend/src/components/contacts/Contact.tsx
@@ -12,7 +12,10 @@ const Contact = ({data}: {data: number}) => {
                 <img className="w-[3.5em] dark:invert dark:contrast-25" src="/user.png" alt="poda" />
                 <span className="absolute border-[3px] border-white dark:border-black bg-green-400 w-3.5 h-3.5 right-1 bottom-1 rounded-full"></span>
             </div>
-            <div className={`font-normal h-[90%] flex items-center w-full`}>Ramu kuttan</div>
+            <div className={`font-normal h-[90%] flex justify-between items-center w-full`}>
+                <p>Ramu kuttan</p>
+                <p className="text-[0.8em] text-gray-400">10:38 pm</p>
+            </div>
         </div>
     )
 };
