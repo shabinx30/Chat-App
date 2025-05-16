666c2455756c3f7cf96789c41d87657f8a92dd8c
shabinx30 <shabeensharih@gmail.com>
2025-05-16 10:00:18 +0530

    feat: add react-router-dom for routing; update App component to include routing and integrate Contacts and Welcome components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useState, type FormEvent } from 'react';
-import { io, Socket } from 'socket.io-client';
-import Home from './pages/Home';
+import { useEffect, useState, type FormEvent } from "react";
+import { io, Socket } from "socket.io-client";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Contacts from "./components/contacts/Contacts";
+import Chat from "./components/chat/Chat";
 
 // Define type for messages
 type Message = string;
@@ -9,50 +13,58 @@ type Message = string;
 let socket: Socket;
 
 function App() {
-  const [message, setMessage] = useState<string>('');
-  const [chat, setChat] = useState<Message[]>([]);
+    const [message, setMessage] = useState<string>("");
+    const [chat, setChat] = useState<Message[]>([]);
 
-  // useEffect(() => {
-  //   socket = io('http://localhost:5004');
-    
-  //   socket.on('chat message', (msg: Message) => {
-  //     setChat((prev) => [...prev, msg]);
-  //   });
+    // useEffect(() => {
+    //   socket = io('http://localhost:5004');
 
-  //   return () => {
-  //     socket.off('chat message');
-  //     socket.disconnect();
-  //   };
-  // }, []);
+    //   socket.on('chat message', (msg: Message) => {
+    //     setChat((prev) => [...prev, msg]);
+    //   });
 
-  // const sendMessage = (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   if (message.trim()) {
-  //     socket.emit('chat message', message);
-  //     setMessage('');
-  //   }
-  // };
+    //   return () => {
+    //     socket.off('chat message');
+    //     socket.disconnect();
+    //   };
+    // }, []);
 
-  return (
-    // <div style={{ padding: 20 }}>
-    //   <h2>Socket.IO Chat</h2>
-    //   <ul>
-    //     {chat.map((msg, index) => (
-    //       <li key={index}>{msg}</li>
-    //     ))}
-    //   </ul>
-    //   {/* <form onSubmit={sendMessage}> */}
-    //   <form >
-    //     <input
-    //       value={message}
-    //       onChange={e => setMessage(e.target.value)}
-    //       placeholder="Type a message..."
-    //     />
-    //     <button type="submit">Send</button>
-    //   </form>
-    // </div>
-    <Home />
-  );
+    // const sendMessage = (e: FormEvent<HTMLFormElement>) => {
+    //   e.preventDefault();
+    //   if (message.trim()) {
+    //     socket.emit('chat message', message);
+    //     setMessage('');
+    //   }
+    // };
+
+    return (
+        // <div style={{ padding: 20 }}>
+        //   <h2>Socket.IO Chat</h2>
+        //   <ul>
+        //     {chat.map((msg, index) => (
+        //       <li key={index}>{msg}</li>
+        //     ))}
+        //   </ul>
+        //   {/* <form onSubmit={sendMessage}> */}
+        //   <form >
+        //     <input
+        //       value={message}
+        //       onChange={e => setMessage(e.target.value)}
+        //       placeholder="Type a message..."
+        //     />
+        //     <button type="submit">Send</button>
+        //   </form>
+        // </div>
+        <>
+            <div className="flex w-[100vw] h-[100vh]">
+                <Contacts />
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/components/contacts/Contact.tsx
+++ frontend/src/components/contacts/Contact.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 
 
 
 const Contact = ({data}: {data: number}) => {
+
+    const navigate = useNavigate()
+
     return (
-        <div className={`w-full h-[4.5em] ${data == 2 ? 'bg-[#eff0ff] dark:bg-[#222222] rounded-2xl' : ''} text-black dark:text-[#eff0ff] flex justify-center gap-4 items-center px-4`}>
+        <div onClick={() => navigate('/chat')} className={`w-full h-[4.5em] ${data == 2 ? 'bg-[#eff0ff] dark:bg-[#222222] rounded-2xl' : ''} text-black dark:text-[#eff0ff] flex justify-center gap-4 items-center px-4`}>
             <div className="relative flex items-center justify-center">
                 <img className="w-[3.5em] dark:invert dark:contrast-25" src="/user.png" alt="poda" />
                 <span className="absolute border-[3px] border-white dark:border-black bg-green-400 w-3.5 h-3.5 right-1 bottom-1 rounded-full"></span>
