ddcb2f3154a154eae24296a19fb262f81517f7f3
shabinx30 <shabeensharih@gmail.com>
2025-05-19 12:29:13 +0530

    refactor: remove unused socket.io code and clean up App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-// import { useEffect, useState, type FormEvent } from "react";
-// import { io, Socket } from "socket.io-client";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./components/Welcome";
@@ -7,55 +5,10 @@ import Chat from "./components/chat/Chat";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
-// Define type for messages
-// type Message = string;
-
-// Define socket type
-// let socket: Socket;
 
 function App() {
-    // const [message, setMessage] = useState<string>("");
-    // const [chat, setChat] = useState<Message[]>([]);
-
-    // useEffect(() => {
-    //   socket = io('http://localhost:5004');
-
-    //   socket.on('chat message', (msg: Message) => {
-    //     setChat((prev) => [...prev, msg]);
-    //   });
-
-    //   return () => {
-    //     socket.off('chat message');
-    //     socket.disconnect();
-    //   };
-    // }, []);
-
-    // const sendMessage = (e: FormEvent<HTMLFormElement>) => {
-    //   e.preventDefault();
-    //   if (message.trim()) {
-    //     socket.emit('chat message', message);
-    //     setMessage('');
-    //   }
-    // };
 
     return (
-        // <div style={{ padding: 20 }}>
-        //   <h2>Socket.IO Chat</h2>
-        //   <ul>
-        //     {chat.map((msg, index) => (
-        //       <li key={index}>{msg}</li>
-        //     ))}
-        //   </ul>
-        //   {/* <form onSubmit={sendMessage}> */}
-        //   <form >
-        //     <input
-        //       value={message}
-        //       onChange={e => setMessage(e.target.value)}
-        //       placeholder="Type a message..."
-        //     />
-        //     <button type="submit">Send</button>
-        //   </form>
-        // </div>
         <Routes>
             <Route path="/" element={<Home aside={<Welcome />} />} />
             <Route path="/chat/:email" element={<Home aside={<Chat />} />} />
