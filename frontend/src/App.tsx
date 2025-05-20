fac4e0ac9ad8c98f7eb7a72b79f292a99ab21f46
shabinx30 <shabeensharih@gmail.com>
2025-05-20 16:55:22 +0530

    feat: integrate Redux for user authentication and handle JWT in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,16 @@ import Welcome from "./components/Welcome";
 import Chat from "./components/chat/Chat";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-
+import { useDispatch } from "react-redux";
+import { login } from "./redux/store";
 
 function App() {
+    const dispatch = useDispatch();
+
+    if (localStorage.getItem("jwt")) {
+        let userData = JSON.parse(localStorage.getItem("jwt") || '')
+        dispatch(login({ token: null, user: userData }));
+    }
 
     return (
         <Routes>
