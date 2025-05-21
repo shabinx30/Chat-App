c27b83594e5e77a4fe195b66ecb730942e73fe0c
shabinx30 <shabeensharih@gmail.com>
2025-05-21 10:45:51 +0530

    feat: update CORS configuration to restrict allowed origins to localhost only

--- backend/src/libs/socket.ts
+++ backend/src/libs/socket.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3003", "http://172.16.3.155:3003/"],
+        origin: ["http://localhost:3003"],
         methods: ['GET', 'POST']
     }
 })
