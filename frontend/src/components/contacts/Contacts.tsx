7eec05bdafb61db140912bd6bfafb19322e37fee
shabinx30 <shabeensharih@gmail.com>
2025-05-28 17:12:05 +0530

    fix: refactor Contacts component to improve search functionality and code readability

--- frontend/src/components/contacts/Contacts.tsx
+++ frontend/src/components/contacts/Contacts.tsx
@@ -1,7 +1,7 @@
 import Contact from "./Contact";
 import { IoMdSettings } from "react-icons/io";
 import { GoPlus } from "react-icons/go";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useSelector, type TypedUseSelectorHook } from "react-redux";
 import type { RootState } from "../../redux/store";
@@ -32,37 +32,42 @@ interface ctcType {
     lastMessageAt: Date;
 }
 
+function debounce(func: (...args: any[]) => void, delay: number) {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    return (...args: any[]) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func(...args), delay);
+    };
+}
+
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const Contacts = ({ change, setPop }: AddContactType) => {
     const [ctc, setCtc] = useState<ctcType[]>([]);
-    const [onUsers, setOnUsers] = useState<Set<string>>(new Set())
-    const [chatMsg, setChatMsg] = useState<string>('')
+    const [onUsers, setOnUsers] = useState<Set<string>>(new Set());
+    const [chatMsg, setChatMsg] = useState<string>("");
     const state = useTypedSelector((state) => state);
 
-    const { socket } = useAppContext()
+    const { socket } = useAppContext();
     const { chatId } = useParams();
 
     //for dev
-    const set = new Set()
-
+    const set = new Set();
+    const { VITE_BASE_URL } = import.meta.env;
     const userData = {
         userId: state.auth.user.userId,
     };
 
     useEffect(() => {
-        setCtc([])
+        setCtc([]);
         const getContacts = () => {
             axios
-                .post(
-                    `${import.meta.env.VITE_BASE_URL}/api/chat/getcontacts`,
-                    userData
-                )
+                .post(`${VITE_BASE_URL}/api/chat/getcontacts`, userData)
                 .then((res) => {
                     for (let con of res.data.chat) {
-                        if(!set.has(con._id)) {
+                        if (!set.has(con._id)) {
                             setCtc((p) => [con, ...p]);
-                            set.add(con._id)
+                            set.add(con._id);
                         }
                     }
                 })
@@ -70,18 +75,45 @@ const Contacts = ({ change, setPop }: AddContactType) => {
                     console.log(error);
                 });
         };
-        getContacts()
-        socket.on("online", res => {
-            setOnUsers(new Set(res))
-        })
-        socket.on("chat message", message => {
-            console.log(message)
-            setChatMsg(message.tosChat)
-        })
-    },[change]);
+        getContacts();
+        socket.on("online", (res) => {
+            setOnUsers(new Set(res));
+        });
+        socket.on("chat message", (message) => {
+            console.log(message);
+            setChatMsg(message.tosChat);
+        });
+    }, [change]);
+
+    const searchRef = useRef<HTMLInputElement>(null);
+
+    const searchContact = (e: React.ChangeEvent<HTMLInputElement>) => {
+        axios
+            .post(
+                `${VITE_BASE_URL}/api/chat/searchcontacts`,
+                e.target.value.trim()
+            )
+            .then((res) => {
+                for (let con of res.data.chat) {
+                    if (!set.has(con._id)) {
+                        setCtc((p) => [con, ...p]);
+                        set.add(con._id);
+                    }
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    };
+
+    const debouncedSearch = useCallback(debounce(searchContact, 500), []);
 
     return (
-        <section className={`${chatId ? 'hidden md:block md:flex-1': 'flex-1'} bg-[#ffffff] h-[100dvh] relative dark:bg-gray-900 text-black dark:border-r border-gray-800`}>
+        <section
+            className={`${
+                chatId ? "hidden md:block md:flex-1" : "flex-1"
+            } bg-[#ffffff] h-[100dvh] relative dark:bg-gray-900 text-black dark:border-r border-gray-800`}
+        >
             <div className="block h-[21vh]">
                 <div className="mx-4 h-[4em] flex justify-between items-center">
                     <h1 className="font-bold text-3xl text-[#626fff]">Chat</h1>
@@ -91,6 +123,8 @@ const Contacts = ({ change, setPop }: AddContactType) => {
                 </div>
                 <div className="flex justify-center mt-[2em] mb-[1em]">
                     <input
+                        ref={searchRef}
+                        onChange={debouncedSearch}
                         className="border border-[#8892ff] bg-[#eff0ff] dark:border-[#414678] dark:bg-gray-800 dark:text-white rounded-2xl w-[94%] outline-none px-4 py-2 placeholder:text-[#818bff]"
                         type="text"
                         placeholder="Search"
@@ -102,7 +136,14 @@ const Contacts = ({ change, setPop }: AddContactType) => {
                     <div></div>
                 ) : (
                     ctc?.map((item, index) => (
-                        <Contact key={index} userId={state.auth.user.userId} onUsers={onUsers} chatMsg={chatMsg} chatId={chatId} data={item} />
+                        <Contact
+                            key={index}
+                            userId={state.auth.user.userId}
+                            onUsers={onUsers}
+                            chatMsg={chatMsg}
+                            chatId={chatId}
+                            data={item}
+                        />
                     ))
                 )}
                 <div
