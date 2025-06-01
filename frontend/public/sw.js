43401fd2bded14c1ec190d954da7f9d63f75af87
shabinx30 <shabeensharih@gmail.com>
2025-06-01 17:42:38 +0530

    feat: enhance push notification handling; add actions and interaction options

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,7 +1,15 @@
 self.addEventListener("push", function (event) {
     const data = event.data.json();
+    // console.log(data)
     self.registration.showNotification(data.title, {
         body: data.body,
         icon: data.icon || "/user.png",
+        actions: [
+            { action: "view", title: "View" },
+            { action: "dismiss", title: "Dismiss" },
+        ],
+        renotify: true,
+        requireInteraction: true,
+        silent: false,
     });
 });
