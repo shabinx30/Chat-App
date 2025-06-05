5c577a50dff22c8693a63ebcee170d2f467b3faa
shabinx30 <shabeensharih@gmail.com>
2025-06-05 22:41:42 +0530

    feat: add edit functionality for user name and email in Settings component with save animation

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -1,5 +1,5 @@
 // import React, { type FormEvent } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useSelector, type TypedUseSelectorHook } from "react-redux";
 import type { RootState } from "../../redux/store";
 import { RxExit } from "react-icons/rx";
@@ -7,26 +7,30 @@ import { MdOutlineModeEditOutline } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/store";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 interface settingsType {
     setSett: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Settings = ({ setSett }: settingsType) => {
-    
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isEdit, setEdit] = useState({
+        name: false,
+        email: false,
+    });
+
     const handleLogout = () => {
-        dispatch(logout())
-        localStorage.removeItem('jwt')
-        navigate('/login')
-    }
-    
+        dispatch(logout());
+        localStorage.removeItem("jwt");
+        navigate("/login");
+    };
+
     // const handleSubmit = (e: FormEvent) => {
     //     e.preventDefault();
     // };
-    
+
     const selector: TypedUseSelectorHook<RootState> = useSelector;
     const state = selector((state) => state);
 
@@ -51,40 +55,92 @@ const Settings = ({ setSett }: settingsType) => {
                 onClick={(e) => e.stopPropagation()}
                 className="w-full bg-white rounded-3xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-[#1b1b1b]/50 dark:border-[#2b2b2b]"
             >
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-gray-900 dark:text-white">
+                <div
+                    onClick={() => setEdit({ name: false, email: false })}
+                    className="p-6 space-y-4 md:space-y-6 sm:p-8 text-gray-900 dark:text-white"
+                >
                     <div className="flex justify-between">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Settings
                         </h1>
-                        <div onClick={handleLogout} className="flex items-center justify-between gap-2 bg-red-400 rounded-2xl cursor-pointer px-3 pt-1 pb-1.5">
+                        <div
+                            onClick={handleLogout}
+                            className="flex items-center justify-between gap-2 bg-red-400 rounded-2xl cursor-pointer px-3 pt-1 pb-1.5"
+                        >
                             <h2 className="dark:text-white text-sm">Log out</h2>
                             <RxExit size={17} className="text-white" />
                         </div>
                     </div>
                     <div className="flex flex-col gap-2 mt-[3em]">
                         <div className="relative flex justify-center group w-fit cursor-pointer">
-                            <MdOutlineModeEditOutline className="absolute opacity-0 self-center group-hover:opacity-100 z-20 " size={18} />
-                            <img className="z-10 w-[5em] rounded-full brightness-100 group-hover:brightness-50 duration-150" src={state.auth.user.profile} alt="user" />
+                            <MdOutlineModeEditOutline
+                                className="absolute opacity-0 self-center group-hover:opacity-100 z-20 "
+                                size={18}
+                            />
+                            <img
+                                className="z-10 w-[5em] rounded-full brightness-100 group-hover:brightness-50 duration-150"
+                                src={state.auth.user.profile}
+                                alt="user"
+                            />
                         </div>
-                        <div className="flex justify-between items-center">
-                            <h1 className="dark:text-white font-bold text-lg">
-                                {state.auth.user.name}
-                            </h1>
-                            <MdOutlineModeEditOutline className="cursor-pointer" size={18} />
+                        <div
+                            onClick={(e) => e.stopPropagation()}
+                            className="flex justify-between items-center"
+                        >
+                            {isEdit.name ? (
+                                <input
+                                    className="font-bold text-lg bg-[#2b2b2b] w-full p-1"
+                                    value={state.auth.user.name}
+                                    type="text"
+                                />
+                            ) : (
+                                <h1 className="dark:text-white font-bold text-lg">
+                                    {state.auth.user.name}
+                                </h1>
+                            )}
+                            {!isEdit.name && (
+                                <MdOutlineModeEditOutline
+                                    onClick={() => {
+                                        setEdit((p) => ({ ...p, name: true }));
+                                    }}
+                                    className="cursor-pointer"
+                                    size={18}
+                                />
+                            )}
                         </div>
-                        <div className="flex justify-between items-center">
-                            <div>
+                        <div
+                            onClick={(e) => e.stopPropagation()}
+                            className="flex justify-between items-center"
+                        >
+                            <div className="flex flex-col w-full">
                                 <label
                                     htmlFor="email"
                                     className="text-gray-500 text-sm font-semibold"
                                 >
                                     Email
                                 </label>
-                                <h2 className="dark:text-white font-semibold">
-                                    {state.auth.user.email}
-                                </h2>
+                                {isEdit.email ? (
+                                    <input
+                                        id="eamil"
+                                        className="bg-[#2b2b2b] font-semibold w-full p-1"
+                                        value={state.auth.user.email}
+                                        type="text"
+                                    />
+                                ) : (
+                                    <h2 className="dark:text-white font-semibold">
+                                        {state.auth.user.email}
+                                    </h2>
+                                )}
                             </div>
-                            <MdOutlineModeEditOutline className="cursor-pointer" size={18} />
+                            {!isEdit.email && (
+                                <MdOutlineModeEditOutline
+                                    onClick={() => {
+                                        setEdit((p) => ({ ...p, email: true }));
+                                    }}
+                                    className="cursor-pointer"
+                                    size={18}
+                                />
+                            )}
                         </div>
                         <div className="flex justify-center items-end h-[4em]">
                             <h2 className="flex items-center cursor-pointer text-sm gap-2 bg-[#2b2b2b] w-fit rounded-2xl px-4 pt-1 pb-1.5">
@@ -92,6 +148,21 @@ const Settings = ({ setSett }: settingsType) => {
                                 <MdOutlineModeEditOutline size={18} />
                             </h2>
                         </div>
+                        <AnimatePresence>
+                            {(isEdit.name || isEdit.email) && (
+                                <div className="flex justify-center">
+                                    <motion.div
+                                        initial={{ height: 0, opacity: 0 }}
+                                        animate={{ height: "auto", opacity: 1 }}
+                                        exit={{ height: 0, opacity: 0 }}
+                                        transition={{duration: .1}}
+                                        className="bg-[#b0ff62] text-black w-fit px-4 pt-1 pb-1.5 rounded-2xl"
+                                    >
+                                        Save
+                                    </motion.div>
+                                </div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
             </motion.div>
