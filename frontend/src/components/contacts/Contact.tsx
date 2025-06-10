1e0777f0f887d3dd36b4134c620e616e775cf8eb
shabinx30 <shabeensharih@gmail.com>
2025-06-11 01:02:28 +0530

    fix: update conditional rendering for Contact component background color based on window width

--- frontend/src/components/contacts/Contact.tsx
+++ frontend/src/components/contacts/Contact.tsx
@@ -63,7 +63,7 @@ const Contact = ({
         <div
             onClick={() => navigate(`/chat/${data._id}`)}
             className={`w-full h-[4.5em] ${
-                chatId == data._id
+                chatId == data._id && window.innerWidth >= 768
                     ? "bg-[#c5ff87] dark:bg-[#b0ff62] text-black hover:bg-[#e8ffd0] hover:dark:bg-[#b0ff62]"
                     : "dark:text-[#eff0ff] hover:bg-[#f0ffe0] hover:dark:bg-[#282828]"
             } duration-200 rounded-4xl text-black flex justify-center gap-4 items-center px-3`}
