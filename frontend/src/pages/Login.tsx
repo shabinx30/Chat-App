d28177cc95d84228cae72aa2c18c00bb6c04fb15
shabinx30 <shabeensharih@gmail.com>
2025-06-29 18:09:07 +0530

    fix: refactor Login component layout and improve form structure for better user experience

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,7 +7,7 @@ import { IoIosCloseCircle } from "react-icons/io";
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
-    const apiUrl = import.meta.env.VITE_BASE_URL
+    const apiUrl = import.meta.env.VITE_BASE_URL;
     const dispatch = useDispatch();
 
     const [formData, setFormData] = useState({
@@ -91,10 +91,11 @@ const Login: React.FC = () => {
             .then((res) => {
                 // console.log("login res", res);
                 if (res.data.message == "success") {
-                    window.localStorage.setItem("jwt", JSON.stringify(res.data.user));
-                    dispatch(
-                        login({ token: null, user: res.data.user })
+                    window.localStorage.setItem(
+                        "jwt",
+                        JSON.stringify(res.data.user)
                     );
+                    dispatch(login({ token: null, user: res.data.user }));
 
                     navigate("/");
                 } else {
@@ -102,13 +103,13 @@ const Login: React.FC = () => {
                 }
             })
             .catch((err) => {
-                if(err.response.data.message == "User is not existing!!!") {
-                    showError('Cannot find the user!, Please sign up')
+                if (err.response.data.message == "User is not existing!!!") {
+                    showError("Cannot find the user!, Please sign up");
                     setTimeout(() => {
                         navigate("/signup");
                     }, 3500);
-                }else{
-                    showError(err.response.data.message || 'An Error Occured.')
+                } else {
+                    showError(err.response.data.message || "An Error Occured.");
                 }
                 console.error(err);
             });
@@ -151,90 +152,106 @@ const Login: React.FC = () => {
                     </div>
                 </div>
             )}
-            <section className="bg-gray-50 dark:bg-black pt-14 md:pt-0">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div className="w-full bg-white rounded-3xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-[#1b1b1b] dark:border-[#2b2b2b]">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Sign in to your account
-                            </h1>
-                            <form
-                                noValidate
-                                className="space-y-4 md:space-y-6"
-                                onSubmit={formSubmission}
-                            >
-                                <div>
-                                    <label
-                                        htmlFor="email"
-                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                    >
-                                        {valid.email.message ? (
-                                            <span className="text-red-500">
-                                                {valid.email.message}
-                                            </span>
-                                        ) : (
-                                            "Your email"
-                                        )}
-                                    </label>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        onChange={validate}
-                                        className={
-                                            valid.email.status
-                                                ? regularClass
-                                                : errorClass
-                                        }
-                                        placeholder="example@company.com"
-                                    />
-                                </div>
-                                <div>
-                                    <label
-                                        htmlFor="password"
-                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                    >
-                                        {valid.password.message ? (
-                                            <span className="text-red-500">
-                                                {valid.password.message}
-                                            </span>
-                                        ) : (
-                                            "Your password"
-                                        )}
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
-                                        onChange={validate}
-                                        placeholder="&34@88$#!"
-                                        className={
-                                            valid.password.status
-                                                ? regularClass
-                                                : errorClass
-                                        }
-                                    />
-                                </div>
-                                <button
-                                    type="submit"
-                                    className="w-full text-black bg-[#b0ff62] hover:bg-[#b0ff62] duration-200 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            <div className="flex flex-col md:flex-row gap-8 md:gap-0">
+                <div className="flex flex-col md:flex-1 justify-center items-center md:mb-[3em] gap-3 pt-4 md:pt-2">
+                    <div className="flex items-center gap-2">
+                        <img src="/icons/logo-small.png" alt="logo" className="w-8 h-8"/>
+                        <h1 className="font-bold text-3xl dark:text-[#fff] text-[#000] leading-tight tracking-tight select-none">
+                            Convo
+                        </h1>
+                    </div>
+                    <p className="font-semibold text-sm">
+                        Sign in to continue using Convo
+                    </p>
+                    <ul className="md:block">
+                        
+                    </ul>
+                </div>
+                <section className="bg-gray-50 flex-1/4 dark:bg-black pt-14 md:pt-0">
+                    <div className="flex flex-col items-center justify-center md:h-screen px-6 py-8 mx-auto lg:py-0">
+                        <div className="w-full bg-white rounded-3xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-[#1b1b1b] dark:border-[#2b2b2b]">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Sign in to your account
+                                </h1>
+                                <form
+                                    noValidate
+                                    className="space-y-4 md:space-y-6"
+                                    onSubmit={formSubmission}
                                 >
-                                    Sign In
-                                </button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet?{" "}
-                                    <Link
-                                        to="/signup"
-                                        className="font-medium text-black dark:text-[#b0ff62] hover:underline cursor-pointer"
+                                    <div>
+                                        <label
+                                            htmlFor="email"
+                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                            {valid.email.message ? (
+                                                <span className="text-red-500">
+                                                    {valid.email.message}
+                                                </span>
+                                            ) : (
+                                                "Your email"
+                                            )}
+                                        </label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            onChange={validate}
+                                            className={
+                                                valid.email.status
+                                                    ? regularClass
+                                                    : errorClass
+                                            }
+                                            placeholder="example@company.com"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label
+                                            htmlFor="password"
+                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                            {valid.password.message ? (
+                                                <span className="text-red-500">
+                                                    {valid.password.message}
+                                                </span>
+                                            ) : (
+                                                "Your password"
+                                            )}
+                                        </label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            onChange={validate}
+                                            placeholder="&34@88$#!"
+                                            className={
+                                                valid.password.status
+                                                    ? regularClass
+                                                    : errorClass
+                                            }
+                                        />
+                                    </div>
+                                    <button
+                                        type="submit"
+                                        className="w-full text-black bg-[#b0ff62] hover:bg-[#b0ff62] duration-200 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     >
-                                        Sign up
-                                    </Link>
-                                </p>
-                            </form>
+                                        Sign In
+                                    </button>
+                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Don’t have an account yet?{" "}
+                                        <Link
+                                            to="/signup"
+                                            className="font-medium text-black dark:text-[#b0ff62] hover:underline cursor-pointer"
+                                        >
+                                            Sign up
+                                        </Link>
+                                    </p>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </>
     );
 };
