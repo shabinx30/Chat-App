e3f7db62cf42bb4a084ef39024f9b6ec693e23e2
shabinx30 <shabeensharih@gmail.com>
2025-07-06 15:13:30 +0530

    fix: simplify subscription logic in useSubscribe hook

--- frontend/src/hooks/useSubscribe.ts
+++ frontend/src/hooks/useSubscribe.ts
@@ -2,39 +2,32 @@ import { useEffect } from "react";
 import { subscribeToPush } from "../utils/push";
 
 const useSubscribe = () => {
-
     const { VITE_PUBLIC_VAPID_KEY, VITE_BASE_URL } = import.meta.env;
 
     useEffect(() => {
         const handleSubscribe = async () => {
-            const registration = await navigator.serviceWorker.ready;
+            const subscription = await subscribeToPush(VITE_PUBLIC_VAPID_KEY);
+            if (subscription) {
+                const user = JSON.parse(localStorage.getItem("jwt") || "{}");
 
-            // Check if user is already subscribed
-            const existingSubscription =
-                await registration.pushManager.getSubscription();
-
-            if (!existingSubscription) {
-                const subscription = await subscribeToPush(
-                    VITE_PUBLIC_VAPID_KEY
-                );
-                if (subscription) {
-                    const user = JSON.parse(
-                        localStorage.getItem("jwt") || "{}"
-                    );
-
-                    await fetch(`${VITE_BASE_URL}/notify/subscribe`, {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            userId: user.userId,
-                            subscription,
-                        }),
-                    });
-                }
+                await fetch(`${VITE_BASE_URL}/notify/subscribe`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        userId: user.userId,
+                        subscription,
+                    }),
+                });
             }
         };
 
-        handleSubscribe();
+        navigator.serviceWorker
+            .getRegistrations()
+            .then(function (registrations) {
+                if (!registrations.length) {
+                    handleSubscribe();
+                }
+            });
     }, []);
 };
 
